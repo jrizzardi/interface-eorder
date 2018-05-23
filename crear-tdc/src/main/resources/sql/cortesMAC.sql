e1cfeb3963e66cbc08b06cebcb9c3e58b0a01bb7
Oscar Vazquez <oscar.a.vazquez@enel.com>
2018-05-23 12:38:02 -0300

    WS Test initial commit

---
+++ crear-tdc/src/main/resources/sql/cortesMAC.sql
@@ -0,0 +1,6 @@
+SELECT c.nro_servicio, c.numero_cliente, c.sucursal,
+       c.sector, c.zona, c.tarifa
+  FROM servicio_cab c JOIN servicio_corte s ON (c.nro_servicio = s.nro_servicio)
+  WHERE c.estado_transmision = 'T'
+    AND c.id_servicio = 1
+  ORDER BY c.nro_servicio DESC
---
+++ ws-test/src/main/java/edesur/eorder/test/ws/TestService.java
@@ -0,0 +1,7 @@
+package edesur.eorder.test.ws;
+
+import javax.jws.WebParam;
+
+public interface TestService {
+//    public void CrearTdC(@WebParam)
+}
